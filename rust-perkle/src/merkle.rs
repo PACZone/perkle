08cab0392e0bf09e6785c239b289b8f72c9c2d9d
Kay <kehiiiiya@gmail.com>
2024-06-26 17:44:49 +0000

    feat(rust): fixing erros, adding invalidate and recaculate

--- rust-perkle/src/merkle.rs
+++ rust-perkle/src/merkle.rs
@@ -6,7 +6,7 @@ use crate::Hasher;
 struct Node<T: Hasher> {
     width: usize,
     height: usize,
-    hashe: Option<T::Hash>,
+    hash: Option<T::Hash>,
 }
 
 pub struct Tree<T: Hasher> {
@@ -35,19 +35,49 @@ impl<T: Hasher> Tree<T> {
         self.nodes.get(&node_id)
     }
 
-    fn get_or_create_node(&mut self, width: usize, height: usize) -> Option<&Node<T>> {
+    fn get_or_create_node(&mut self, width: usize, height: usize) -> Node<T> {
         let node_id = node_id(width, height);
-        let node = self.get_node(node_id);
-        if let Some(n) = node {
-            return Some(n);
-        }
 
-        let new_node = Node {
-            width,
-            height,
-            hashe: None,
-        };
-
-        // self.nodes.insert(node_id, new_node).as_ref() //! Compiler error.
+        self.nodes
+            .entry(node_id)
+            .or_insert(Node {
+                width,
+                height,
+                hash: None,
+            })
+            .clone()
     }
+
+    fn invalidate_node(&mut self, width: usize, height: usize) {
+        let mut node = self.get_or_create_node(width, height);
+        node.hash = None;
+    }
+
+    fn recalculate_height(&mut self, max_width: usize) {
+        if max_width > self.max_width {
+            self.max_width = max_width;
+
+            let max_height = (max_width as f64).log2(); // should we ude `f32` here?
+
+            if max_height.rem_euclid(1.0) != 0.0 {
+                self.max_height = (max_height.trunc() + 2.0) as usize;
+            } else {
+                self.max_height = (max_height.trunc() + 1.0) as usize;
+            }
+        }
+    }
+
+    // WIP!
+    // fn node_hash(&mut self, width: usize, height: usize) -> Option<T::Hash> {
+    //     let n_id = node_id(width, height);
+
+    //     let node = self.get_node(n_id);
+    //     if node.is_none() {
+    //         let n_id = node_id(width, height);
+    //         let node = self.get_node(n_id);
+    //         if node.is_none() {
+    //             return None;
+    //         }
+    //     }
+    // }
 }
